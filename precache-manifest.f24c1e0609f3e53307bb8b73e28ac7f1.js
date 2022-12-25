2d144ed7e84b97dd04c5734cf933f90555d4b24a
vladimirk <zukikara@gmail.com>
2022-12-25 22:08:00 +0100

    Updates

---
+++ precache-manifest.f24c1e0609f3e53307bb8b73e28ac7f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545ff357a06395f35fb804a94aa6191c",
+    "url": "/post-list/index.html"
+  },
+  {
+    "revision": "73a7da75f40757b6cf7b",
+    "url": "/post-list/static/css/main.6d02094a.chunk.css"
+  },
+  {
+    "revision": "9c1018acfb67e84f2e04",
+    "url": "/post-list/static/js/2.d94b63f6.chunk.js"
+  },
+  {
+    "revision": "73a7da75f40757b6cf7b",
+    "url": "/post-list/static/js/main.f0c7e63c.chunk.js"
+  },
+  {
+    "revision": "70af37f564d566f5cb00",
+    "url": "/post-list/static/js/runtime~main.68ccdd48.js"
+  },
+  {
+    "revision": "9443022862e1bc78a0f7db164e5902aa",
+    "url": "/post-list/static/media/cover_photo.94430228.jpg"
+  },
+  {
+    "revision": "3e08fc762c50d9134f512e9ec7ebd32d",
+    "url": "/post-list/static/media/gooey-ball-tail-spinner.3e08fc76.gif"
+  }
+]);
